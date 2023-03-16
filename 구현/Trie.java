6f114d2cfde2ae40752552bf5a7fc2b8be7e87f4
JavaYuhm <99159721+JavaYuhm@users.noreply.github.com>
2023-03-16 15:56:10 +0900

    [feat] Trie Leetcode 문제 20230316

---
+++ 구현/Trie.java
@@ -0,0 +1,50 @@
+public class Trie {
+    private static class TrieNode {
+        TrieNode[] children;
+        boolean isEnd;
+
+        TrieNode() {
+            this.children = new TrieNode[26];
+        }
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchNode(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchNode(prefix);
+        return node != null;
+    }
+
+    private TrieNode searchNode(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
